2eaa519825b3f05ed1cf8b8e466b0fc49336f0fc
ryutah <ryuta0221@gmail.com>
2020-03-06 23:43:01 +0900

    feat(hello-next): adds sample

---
+++ hello-next/components/MyLayout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Header from './Header';
+
+const layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD',
+};
+
+const Layout = ({ children }) => (
+  <div style={layoutStyle}>
+    <Header />
+    {children}
+  </div>
+);
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
